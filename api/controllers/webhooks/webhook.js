1bfe4f199f795c2b745e11de957d9e5ead12dc67
Handres De La Torre <jack6884ha@gmail.com>
2021-09-21 17:32:19 -0500

    Api en estado OK- se creo el archivo de prueba

--- api/controllers/webhooks/webhook.js
+++ api/controllers/webhooks/webhook.js
@@ -5,144 +5,11 @@ const { httpError } = require('../../helpers/handleError')
 //const mautic = require('../mautic/apimautic')
 const odooApi = require('../odoo/odooapi')
 
-// const crypto = require('crypto-js');
-// const sha256 = require ('crypto-js/sha256');
-// const hmacSHA256 = require ('crypto-js/hmac-sha512');
-// const Base64 = require ('crypto-js/enc-base64');
-// const crypto = require('crypto');
-// const getRawBody = require('raw-body');
-// const { error } = require('console');
-
-const SECRET = '3dfd764e1cc1a4e3de45e220e174f4655d9b99a65f36882bc7a989fcf41b3413'
-
-//const message, nonce, path, privateKey; // ...
-//const hashDigest = sha256(nonce + message);
-//const hmacDigest = Base64.stringify(hmacSHA256(path + hashDigest, privateKey));
-
-const Listener = async (req, res) => {
-    try {
-        const data = req.body;
-        const name = data['sessionInfo']['parameters']['person'][0]['name'];
-        const email = data['sessionInfo']['parameters']['correo-usuario'][0];
-        const mobile = data['sessionInfo']['parameters']['mobile'][0];
-        mautic.createUserWithChat(name, email, mobile)
-        res.send({ "completo": "200" })
-        console.log({ usuario: "nuevo confirmado" });
-    } catch (e) {
-        httpError(res, e)
-    }
-}
-
 const respuesta = (req, res) => {
     res.send(`<h1>Funciona el webhook</h1>`)
 }
 
-const listener2 = (req, res) => {
-    try {
-
-        const body = JSON.stringify(req.body);
-        const header = req.headers;
-        const headerLength = req.headers['content-length'];
-
-        const computedSignature = crypto.createHmac('sha256', SECRET)
-            .update(body)
-            .digest('base64');
-
-        console.log('Computed signature (from body):', computedSignature);
-
-
-        console.log('***---> Body: ');
-
-        //console.log('***---> Header length: ', headerLength);
-        res.send({ "completo": "200" });
-        console.log('***---> Header full: ', header);
-    } catch (e) {
-        httpError(res, e)
-    }
-};
-
-const listenertest = (req, res) => {
-    try {
-        const data = req.body;
-        odooApi.createContact(req)
-        res.send({ "completo": "200", 'webhook': 'TEST' });
-    } catch (e) {
-        httpError(res, e)
-    }
-}
-
-var peticiones = []
-
-const test2 = (req, res) => {
-    try {
-        data = req.body.data
-
-        const totalPeticiones = peticiones.push(data)
-        console.log(peticiones);
-        console.log(totalPeticiones);
-
-        if (totalPeticiones === 3) {
-            peticiones = []
-            let contador = 0;
-            contador++
-            var conteofinal = +1
-            console.log(`Data es: ${data} && contador es ${contador}`);
-        } else {
-            contador = 0
-        }
-
-        res.send({ "completo": "200", 'webhook': 'TEST' });
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-/**
- * Webhook para busqueda de contacto en odoo por email
- */
-const searchContactByemail = async (req, res) => {
-    let points = parseInt(req.body['mautic.lead_points_change'][0]['points']['new_points'], 10);
-    let tags = req.body['mautic.lead_points_change'][0]['contact']['tags']
-    let data = req.body['mautic.lead_points_change'][0]['contact']['fields']['core'];
-    let name = data.firstname.normalizedValue
-    let lastname = data.lastname.normalizedValue
-    let fullname = name + ' ' + lastname
-    let mobile = data.mobile.normalizedValue
-    let country = data.country.normalizedValue
-    let email = data.email.normalizedValue
-    try {
-        const busquedaUsuarioOdoo = await odooApi.searchContact(email)
-        const busquedaLeadOdoo = await odooApi.searchLead(email)
-        const countryId = await odooApi.searchCountry(country)
-        if (busquedaUsuarioOdoo.length === 0 && busquedaLeadOdoo.length === 0) {
-            if (points >= 25 && points <= 50) {
-                const contactoIdCreate = await odooApi.createContact(fullname, email, parseInt(countryId, 10), mobile);
-                console.log(`***==>Se creo un nuevo contacto el id es: ${contactoIdCreate}`);
-            } else console.log(`**==> No existe el contacto pero necesita entre 25 y 50 puntos para ser creado solo tiene ${points}`);
-
-        } else if (busquedaLeadOdoo.length === 0 && busquedaUsuarioOdoo.length >= 1) {
-            if (points >= 51 && points <= 80) {
-                console.log(`***==>El Lead se puede crear ahora `);
-                const leadCreated = await odooApi.createLead(parseInt(busquedaUsuarioOdoo, 10), fullname, mobile);
-                console.log(`****=>Se Creo el Lead nº ${leadCreated}`);
-            } else {
-                console.log(`****==>El lead necesita mas de 51 puntos; tan solo tiene ${points}`);
-            }
-        } else {
-            console.log(`No se ejecuto ningun metodo el usuario solo tiene ${points} puntos`);
-            //const updateDataLead = await odooApi.updateLeads(parseInt(busquedaLeadOdoo, 10))
-            //console.log(updateDataLead);
-            const dataLead = await odooApi.readLeads(busquedaLeadOdoo);
-            console.log(`***La Data del Lead es: ${JSON.stringify(dataLead)}`);
-        }
-        res.send({ "completo": "200", "webhook": "TEST" });
-    } catch (e) {
-        httpError(res, e)
-    }
-}
-
 let turnos = []
-
 
 /**
  * 
@@ -214,12 +81,20 @@ const pointschange = async (req, res) => {
     }
 }
 
+const postPruebas = async (req, res) => {
+    try {
+        const dataHeaders = req.headers
+        const dataBody = req.body
+        console.log(dataHeaders);
+        console.log(dataBody);
+        res.send({ "completo": "200", "webhook": "TEST" });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 module.exports = {
-    test2,
-    respuesta,
-    listener2,
-    listenertest,
-    searchContactByemail,
     pointschange,
-    Listener
+    postPruebas,
+    respuesta
 }
--- api/routes/webhook.js
+++ api/routes/webhook.js
@@ -6,18 +6,14 @@
  //const checkOrigin = require('../middleware/origin')
  const webhook = require('../controllers/webhooks/webhook')
  //webhook inicia
- router.post('/callback', webhook.listener2);
  
- router.post('/callbacktest', webhook.listenertest);
- 
- router.post('/callbacktest2', webhook.searchContactByemail);
- 
+ router.post('/pruebas', webhook.postPruebas)
+
  router.post('/points', webhook.pointschange)
  
- //router.get('/callbacktest2', webhook.searchContactByemail);
  
  router.get('/', webhook.respuesta)
  
  
  
- module.exports = router 
+ module.exports = router
